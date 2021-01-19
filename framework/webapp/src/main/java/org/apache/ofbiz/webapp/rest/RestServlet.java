9730a4b4acb8c09d443bbdc9a16496b1a01f7471
Chatree Srichart <chatree.srichart@gmail.com>
2021-01-19 22:46:52 +0700

    Inititialize RestServlet.onGet method

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/rest/RestServlet.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/rest/RestServlet.java
@@ -1,18 +1,39 @@
 package org.apache.ofbiz.webapp.rest;
 
-import org.apache.juneau.rest.RestContextBuilder;
+import org.apache.juneau.rest.RestContext;
+import org.apache.juneau.rest.RestRequest;
+import org.apache.juneau.rest.annotation.RestMethod;
+import org.apache.juneau.rest.util.UrlPathPattern;
+import org.apache.juneau.rest.util.UrlPathPatternMatch;
+import org.apache.ofbiz.base.util.Debug;
 
-import javax.servlet.ServletException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.juneau.http.HttpMethod.GET;
 
 public class RestServlet extends org.apache.juneau.rest.RestServlet {
 
-    @Override
-    public synchronized void setContext(org.apache.juneau.rest.RestContext context) throws ServletException {
-        try {
-            RestContextBuilder builder = org.apache.juneau.rest.RestContext.create(this);
-            super.setContext(new RestContext(this, builder, context));
-        } catch (Exception e) {
-            throw new ServletException(e);
+    private static final String MODULE = RestServlet.class.getName();
+
+    @RestMethod(method = GET, path = "*")
+    public void onGet(RestContext restContext) {
+        RestRequest restRequest = restContext.getRequest();
+        String pathInfo = restRequest.getPathInfo();
+        Debug.logInfo("GET: " + restContext, MODULE);
+
+        String pattern = "/test/{param1}/{param2}";
+        UrlPathPattern urlPathPattern = new UrlPathPattern(pattern);
+        UrlPathPatternMatch urlPathPatternMatch = urlPathPattern.match(pathInfo);
+        if (urlPathPatternMatch != null) {
+
         }
+
+        Map<String, Object> output = new HashMap<>();
+
+        // TODO set fields returned from calling an event
+        output.put("text", "Hello world!");
+
+        restContext.getResponse().setOutput(output);
     }
 }
