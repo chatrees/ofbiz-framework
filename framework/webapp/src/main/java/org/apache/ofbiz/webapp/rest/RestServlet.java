506d3a02cb1b2e2ba582816005bbc09da6a6b3e8
Chatree Srichart <chatree.srichart@gmail.com>
2021-01-20 21:53:27 +0700

    Delete @Rest annotation from RestServlet class

--- framework/webapp/src/main/java/org/apache/ofbiz/webapp/rest/RestServlet.java
+++ framework/webapp/src/main/java/org/apache/ofbiz/webapp/rest/RestServlet.java
@@ -1,15 +1,11 @@
 package org.apache.ofbiz.webapp.rest;
 
-import org.apache.juneau.annotation.BeanConfig;
+import org.apache.juneau.dto.swagger.Info;
 import org.apache.juneau.dto.swagger.Swagger;
-import org.apache.juneau.dto.swagger.ui.SwaggerUI;
-import org.apache.juneau.jsonschema.annotation.JsonSchemaConfig;
+import org.apache.juneau.dto.swagger.SwaggerBuilder;
 import org.apache.juneau.rest.RestContext;
 import org.apache.juneau.rest.RestRequest;
-import org.apache.juneau.rest.annotation.HtmlDoc;
-import org.apache.juneau.rest.annotation.Rest;
 import org.apache.juneau.rest.annotation.RestMethod;
-import org.apache.juneau.rest.annotation.RestResource;
 import org.apache.juneau.rest.util.UrlPathPattern;
 import org.apache.juneau.rest.util.UrlPathPatternMatch;
 import org.apache.ofbiz.base.util.Debug;
@@ -18,14 +14,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.apache.juneau.http.HttpMethod.GET;
-import static org.apache.juneau.http.HttpMethod.OPTIONS;
 
-@Rest(
-
-        // Allow OPTIONS requests to be simulated using ?method=OPTIONS query parameter.
-        allowedMethodParams="OPTIONS"
-)
-public class RestServlet extends org.apache.juneau.rest.RestServlet {
+public class RestServlet extends org.apache.juneau.rest.BasicRestServlet {
 
     private static final String MODULE = RestServlet.class.getName();
 
@@ -50,48 +40,16 @@ public class RestServlet extends org.apache.juneau.rest.RestServlet {
         restContext.getResponse().setOutput(output);
     }
 
-    /**
-     * [OPTIONS /*] - Show resource options.
-     *
-     * @param req The HTTP request.
-     * @return A bean containing the contents for the OPTIONS page.
-     */
-    @RestMethod(name=OPTIONS, path="/*",
 
-            summary="Swagger documentation",
-            description="Swagger documentation for this resource.",
-
-            htmldoc=@HtmlDoc(
-                    // Override the nav links for the swagger page.
-                    navlinks={
-                            "back: servlet:/",
-                            "json: servlet:/?method=OPTIONS&Accept=text/json&plainText=true"
-                    },
-                    // Never show aside contents of page inherited from class.
-                    aside="NONE"
-            )
-    )
-    @JsonSchemaConfig(
-            // Add descriptions to the following types when not specified:
-            addDescriptionsTo="bean,collection,array,map,enum",
-            // Add x-example to the following types:
-            addExamplesTo="bean,collection,array,map",
-            // Don't generate schema information on the Swagger bean itself or HTML beans.
-            ignoreTypes="Swagger,org.apache.juneau.dto.html5.*",
-            // Use $ref references for bean definitions to reduce duplication in Swagger.
-            useBeanDefs="true"
-    )
-    @BeanConfig(
-            // When parsing generated beans, ignore unknown properties that may only exist as getters and not setters.
-            ignoreUnknownBeanProperties="true",
-            // POJO swaps to apply to all serializers/parsers on this method.
-            swaps={
-                    // Use the SwaggerUI swap when rendering Swagger beans.
-                    // This is a per-media-type swap that only applies to text/html requests.
-                    SwaggerUI.class
-            }
-    )
-    public Swagger getOptions(RestRequest restRequest) {
-        return restRequest.getSwagger();
+    @Override
+    public Swagger getOptions(RestRequest req) {
+        // TODO custom Swagger
+        Info info = new Info();
+        info.setTitle("Test 1");
+        info.setVersion("1.0");
+        // FIXME NullPointerException
+        Swagger swagger = SwaggerBuilder.swagger()
+                .basePath("/basePath");
+        return req.getSwagger();
     }
 }
